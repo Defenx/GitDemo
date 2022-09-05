3e9dd6af2cd3a20266c46bcf74f523db62e9a75d
Defenx <70909588+Defenx@users.noreply.github.com>
2022-09-05 16:43:45 +0300

    add multiply

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -9,4 +9,8 @@ public class Calculator {
   public static double sum(double a, double b) {
     return a + b;
   }
+
+  public static double multiply(double a, double b) {
+    return a * b;
+  }
 }
