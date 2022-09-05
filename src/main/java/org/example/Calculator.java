4237cfc8ea9e1bc042b40eaff83e5a65c398eb8e
Jaraxxuss <45173360+Jaraxxuss@users.noreply.github.com>
2022-09-05 16:29:31 +0300

    rebase

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,11 +3,11 @@ package org.example;
 public class Calculator {
   
   public static double divide(double num, double delimetr) {
-    return num / delimetr;
+      return num / delimetr;
   }
 
   public static double sum(double a, double b) {
-    return a + b;
+      return a + b;
   }
 
   // todo: test
