152c8c37299e81bbb97f46284e15a8c153ded329
Jaraxxuss <45173360+Jaraxxuss@users.noreply.github.com>
2022-09-05 16:28:50 +0300

    Update Calculator.java

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,10 +3,14 @@ package org.example;
 public class Calculator {
   
   public static double divide(double num, double delimetr) {
-    return num / delimetr;
+      return num / delimetr;
   }
 
   public static double sum(double a, double b) {
-    return a + b;
+      return a + b;
+  }
+  
+  public static double substract(double a, double b) {
+      return a - b;
   }
 }
