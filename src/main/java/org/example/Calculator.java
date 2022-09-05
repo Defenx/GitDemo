015093f21fe672838f773a24998d47510a49a1c4
Defenx <70909588+Defenx@users.noreply.github.com>
2022-09-05 16:41:09 +0300

    Revert "Update Calculator.java"
    This reverts commit 152c8c37299e81bbb97f46284e15a8c153ded329.

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,14 +3,10 @@ package org.example;
 public class Calculator {
   
   public static double divide(double num, double delimetr) {
-      return num / delimetr;
+    return num / delimetr;
   }
 
   public static double sum(double a, double b) {
-      return a + b;
-  }
-  
-  public static double substract(double a, double b) {
-      return a - b;
+    return a + b;
   }
 }
